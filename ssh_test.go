64f8c6748ed159c3b8c28810b62765ff3627b011
Minghe Huang <Minghe.Huang@booking.com>
2020-03-17 11:40:33 +0800

    connection support timeout add "Connectable" api enable default user "root"

--- ssh_test.go
+++ ssh_test.go
@@ -3,10 +3,43 @@ package ssh
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"testing"
+	"time"
 )
 
 func TestSSH(t *testing.T) {
+	t.Run("Connectable", func(t *testing.T) {
+		cases := []struct {
+			host        string
+			connectable bool
+		}{{
+			host:        "127.0.0.1",
+			connectable: true,
+		},
+			{
+				host:        "192.3.0.1",
+				connectable: false,
+			},
+		}
+		for _, c := range cases {
+			client := New(c.host).
+				WithUser("root").
+				WithPort("2222").
+				WithKey("./test/id_rsa")
+			fmt.Println(client)
+			ok, err := client.Connectable(5 * time.Second)
+			if !ok {
+				if err == nil {
+					t.Fatalf("error should not be nil ")
+				}
+			}
+			if ok != c.connectable {
+				t.Fatalf("should get %v but got %v", c.connectable, ok)
+			}
+		}
+	})
+
 	t.Run("public key", func(t *testing.T) {
 		cases := []struct {
 			cmd    string
